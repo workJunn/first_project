dfbbf1d0c8cb3da56617736160812f6daadb6880
[WorkJunn] <[work.jun@mail.ru]>
2025-06-18 19:56:03 +0300

    5. database

--- database/migrations/2025_06_14_145911_create_sity_table.php
+++ database/migrations/2025_06_14_145911_create_sity_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -14,11 +15,32 @@ return new class extends Migration
         Schema::create('sity', function (Blueprint $table) {
             $table->id()->primary();
             $table->string("name_sity")->nullable();
-            $table->string("latintude", 10, 7)->nullable();# Широта 
-            $table->string("longitude", 10, 7)->nullable();# Долгота 
+            $table->string("latintude")->nullable();# Широта 
+            $table->string("longitude")->nullable();# Долгота 
             $table->timestamps();
         });
+
+        DB::table('sity')->insert([
+            'name_sity' => 'Балашиха',
+            'latintude' => '55°48′00″',
+            'longitude' => '37°56′00″'
+        ]);
+
+
+        DB::table('sity')->insert([
+            'name_sity' => 'Химки',
+            'latintude' => '55°53′21″',
+            'longitude' => '37°26′42″'
+        ]);
+
+
+        DB::table('sity')->insert([
+            'name_sity' => 'Москва',
+            'latintude' => '55°45′02″',
+            'longitude' => '37°37′03″'
+        ]);
     }
+
 
     /**
      * Reverse the migrations.
@@ -28,3 +50,4 @@ return new class extends Migration
         Schema::dropIfExists('sity');
     }
 };
+
