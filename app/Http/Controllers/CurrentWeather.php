3a6c155d71e170c9a6f17a37a7c76fd7bc6160f7
[WorkJunn] <[work.jun@mail.ru]>
2025-06-25 19:25:40 +0300

    5 задание почти

--- app/Http/Controllers/CurrentWeather.php
+++ app/Http/Controllers/CurrentWeather.php
@@ -5,38 +5,43 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\JsonResponse; 
 
 class CurrentWeather extends Controller 
 {
-    public function index(Request $request) {
+    public function index(Request $request)  {
 
-        $lat = 55.7506;
-        $lon = 37.6175;
+        $lat = 59.93;
+        $lon = 10.72;
 
         $key = "Weather_{$lat}_{$lon}";
 
-        $api = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat={$lat}&lon={$lon}.72&altitude=90";
-        $response = Http::get($api);
+        if(cache::has($key)){
+            $Weather = Cache::get($key);
+            return response()->json($Weather);
+        }
+
+        $api = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat={$lat}&lon={$lon}&altitude=90";
+        $response = Http::withHeaders([
+            'User-Agent' => 'MyWeatherApp/1.0 (your_email@example.com)', 
+            'Accept' => 'application/json', // сразу получаем ответ в json формате 
+        ])->get($api);
+
         // dd($response);
         // print_r($response);
-        echo $response;
+        // echo $response;
 
         if ($response->successful()) {
-            $Weather = $response->json();
-            
-            if(cache::has($Weather)){
-                Cache::put($key, $Weather, $seconds = 3600);
-            }
+            $Weather = $response->json();// это переделывает json щтвет в удобный для чтения php - массив 
 
-            return $Weather;
+            Cache::put($key, $Weather, $seconds = 3600);
+
+            return response()->json($Weather);
             
         } else {
             $statusCode = $response->status();
             $errorMessage = $response->body();
+
         }
     }
-    // 1. Сделать запрос в api есть 
-    // 2. сделать провеку на наличие данных 
-    // 3. Ответ в формате json (P.S. хз как)
-    // 4. ответ в кэш (P.S. хз как)
 }
